29fe2f34b9f67e3b979d86a96737e3bdc2cbbe42
tαngenx <me@tangenx.dev>
2026-04-20 01:14:36 +0300

    ДАРТ ПОБЕДА shit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ДАРТ ПОБЕДА',
       theme: ThemeData(
         textTheme: const TextTheme(
           bodyLarge: TextStyle(
